3b7789700ffde2ce79ae3d89b722b658d4f152bc
Tino Thamjarat <tino@oozou.com>
2017-01-27 00:40:53 +0700

    turbolink load competition js

--- app/assets/javascripts/competition.js
+++ app/assets/javascripts/competition.js
@@ -1,6 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
   $('.btn-compet').on('click', function(e){
     value = e.target.innerHTML
     $('#competition-name').val(value)
