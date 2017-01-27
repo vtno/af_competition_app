205d994d39c22d5f1e72d77d22397a8ec15f1c08
Tino Thamjarat <tino@oozou.com>
2017-01-27 20:51:46 +0700

    Prevent precompile fail

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+# Prevent asset precompile fail on Heroku
+config.assets.initialize_on_precompile = false
 
 module AfCompetitionApp
   class Application < Rails::Application
